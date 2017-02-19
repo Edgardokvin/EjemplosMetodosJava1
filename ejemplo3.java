682c39dae2de839cdabbddfcc9496fed88d3588c
Edgardokvin <162t0318@itsm.edu.mx>
2017-02-18 22:26:54 -0600

    Add files via upload

---
+++ ejemplo3.java
@@ -0,0 +1,26 @@
+package ejemplo1;
+
+public class ejemplo3 {
+	public static void main (String args[]){
+		restar();
+		multiplicar();
+		}
+			
+			public static void restar( ){
+					int x=500;
+					int y=730;
+					int restar = x+y;
+					
+					System.out.println("El resutado  de la resta es: "+restar);
+					}
+
+
+	
+	public static void multiplicar( ){
+			int x=10;
+			int y=7;
+			int multiplicar= x*y;
+			
+			System.out.println("Eel resutado  de la multiplicacion es: "+multiplicar);
+			}
+}
