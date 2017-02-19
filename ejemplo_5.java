6519c8cedeaa67fd24b01ef937ab902b92d34132
Edgardokvin <162t0318@itsm.edu.mx>
2017-02-18 22:28:59 -0600

    Add files via upload

---
+++ ejemplo_5.java
@@ -0,0 +1,26 @@
+package ejemplo1;
+
+public class ejemplo_5 {
+	public static void main (String args[]){
+		restar();
+		sumar();
+		}
+			
+			public static void restar( ){
+					int x=500;
+					int y=730;
+					int restar = x+y;
+					
+					System.out.println("El resutado  de la resta es: "+restar);
+					}
+
+
+	
+	public static void sumar( ){
+			int x=10;
+			int y=7;
+			int sumar= x+y;
+			
+			System.out.println("Eel resutado  de la suma es: "+sumar);
+			}
+}
