af249ff7f6af058e354d1ec3772d15288c27f34d
Edgardokvin <162t0318@itsm.edu.mx>
2017-02-18 22:30:00 -0600

    Add files via upload

---
+++ ejemplo_2.java
@@ -0,0 +1,19 @@
+package ejemplo1;
+
+public class ejemplo_2 {
+	public static void main (String args[]){
+	   multiplicar();
+
+		}
+			
+			public static void multiplicar( ){
+					int x=10;
+					int y=7;
+					int multiplicar= x*y;
+					
+					System.out.println("Eel resutado  de la multiplicacion es: "+multiplicar);
+					}
+		}
+
+
+
---
+++ ejemplo_4.java
@@ -0,0 +1,27 @@
+package ejemplo1;
+
+public class ejemplo_4 {
+	public static void main (String args[]){
+		dividir();
+		multiplicar();
+		}
+			
+			public static void dividir( ){
+					int x=4000;
+					int y=730;
+					int dividir = x/y;
+					
+					System.out.println("El resutado  de la divicion es: "+dividir);
+					}
+
+
+	
+	public static void multiplicar( ){
+			int x=1500;
+			int y=7;
+			int multiplicar= x*y;
+			
+			System.out.println("Eel resutado  de la multiplicacion es: "+multiplicar);
+			}
+}
+
