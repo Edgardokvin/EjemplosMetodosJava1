1cb653c0ff82864736e711fb4791f4e85f468a8a
Edgardokvin <162t0318@itsm.edu.mx>
2017-02-18 22:24:25 -0600

    Add files via upload

---
+++ sin_retormo_1.java
@@ -0,0 +1,16 @@
+ package ejemplo1;
+
+public class sin_retormo_1 {
+public static void main (String args[]){
+sumar();
+
+}
+	
+	public static void sumar( ){
+			int x=8;
+			int y=7;
+			int suma = x+y;
+			
+			System.out.println("Eel resutado  de la suma es: "+suma);
+			}
+}
